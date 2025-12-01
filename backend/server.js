46db7ddb46508982f25c815018579dac929fa8b0
Futurestar7891 <gajanandmali69@gmail.com>
2025-12-02 03:00:55 +0530

    about done

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,8 @@ const app = express();
 
 app.use(
   cors({
-    // origin: "https://shopx-omega.vercel.app",
-    origin: "http://localhost:5173",
+    origin: "https://shopx-omega.vercel.app",
+    // origin: "http://localhost:5173",
     credentials: true,
   })
 );
